eef6aed798080e06febc1fb548cd12f434b7ea92
vuminhkhang1995 <vuminhkhang1995@gmail.com>
2015-09-04 12:29:51 +0700

    Yelp
    Basic Setups: Done!

---
+++ Yelp/BusinessesViewController.swift
@@ -0,0 +1,83 @@
+//
+//  BusinessesViewController.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 4/23/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, FiltersViewControllerDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var businesses: [Business]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+//            self.businesses = businesses
+//            
+//            for business in businesses {
+//                println(business.name!)
+//                println(business.address!)
+//            }
+//        })
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        
+//        Business.searchWithTerm("", sort: .Distance, categories: [], deals: nil) { (businesses: [Business]!, error: NSError!) -> Void in
+//            
+//            self.businesses = businesses
+//            self.tableView.reloadData()
+//            
+//            for business in businesses {
+//                println(business.name!)
+//                println(business.address!)
+//            }
+//        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses == nil {
+            return 0
+        } else {
+            return businesses!.count
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
+        
+        cell.business = businesses[indexPath.row]
+        
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    // MARK: - Navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let navigationController = segue.destinationViewController as! UINavigationController
+        let filtersViewController = navigationController.topViewController as! FiltersViewController
+        
+        filtersViewController.delegate = self
+    }
+    
+    func filtersViewController(filltersViewController: FiltersViewController, didUpdateFilters filters: [String : AnyObject]) {
+        
+        var categories = filters["categories"] as? [String]
+        Business.searchWithTerm("Restaurants", sort: nil, categories: categories, deals: nil) { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData()
+        }
+    }
+}
